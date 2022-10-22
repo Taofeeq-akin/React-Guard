85a03b5da85dea8fad93847dfe00b30eb7d7a6d6
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-10-22 18:12:05 +0100

    Use State hook once instaed 0f multiple time for input changes

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,19 +3,38 @@ import React, { useState } from "react";
 import "./NewExpense.css";
 
 const NewExpense = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
+  //   const [enteredTitle, setEnteredTitle] = useState("");
+  //   const [enteredAmount, setEnteredAmount] = useState("");
+  //   const [enteredDate, setEnteredDate] = useState("");
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmout: "",
+    enteredDate: "",
+  });
+
   const titleChangedHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
   };
 
-  const [enteredAmount, setEnteredAmount] = useState("");
   const amountChangedHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    // setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
   };
 
-  const [enteredDate, setEnteredDate] = useState("");
   const dateChangedHandler = (event) => {
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
   };
 
   return (
