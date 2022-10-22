3dab1ea4977f5f1c79f37551400ac04018554aeb
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-10-22 17:58:20 +0100

    Multiple useState to for inputs

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./NewExpense.css";
 
 const NewExpense = () => {
-  const inputClickHandler = (event) => {
-    console.log(event.target.value);
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const titleChangedHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const amountChangedHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const [enteredDate, setEnteredDate] = useState("");
+  const dateChangedHandler = (event) => {
+    setEnteredDate(event.target.value);
   };
 
   return (
@@ -13,15 +24,25 @@ const NewExpense = () => {
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" onChange={inputClickHandler} />
+            <input type="text" onChange={titleChangedHandler} />
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" />
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              onChange={amountChangedHandler}
+            />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2021-05-25" max="2024-12-31" />
+            <input
+              type="date"
+              min="2021-05-25"
+              max="2024-12-31"
+              onChange={dateChangedHandler}
+            />
           </div>
           <div className="new-expense__actions">
             <button type="submit">Add Expense</button>
