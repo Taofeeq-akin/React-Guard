4f0c864ed6a94b6cec38b434247f8dd1798c6bb6
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-10-27 00:51:04 +0100

    set maxValue

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,13 +3,16 @@ import "./Chart.css";
 import ChartBar from "./ChartBar.js";
 
 const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value); // this will give us value in array and no more in object
+  const totalMaximum = Math.max(...dataPointValues);
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoints) => (
         <ChartBar
           key={dataPoints.label}
           value={dataPoints.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoints.label}
         />
       ))}
