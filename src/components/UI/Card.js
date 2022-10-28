eeb03aef845fbf3ea6293ecd05624adb51e6f5af
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-10-28 14:10:28 +0100

    First commit

--- src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import "./Card.css";
-
-function Card(props) {
-  const classes = "card " + props.className; // to makees elemts tag inside see thier style class name
-
-  return <div className={classes}>{props.children}</div>; //props.children will make the code inside Card component works cus other elements can not be put inside
-}
-
-export default Card;
