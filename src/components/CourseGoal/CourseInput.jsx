4e6ddae363099a97f395c64966a95bfcceae0675
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-10-30 00:03:24 +0100

    Fetch input valuse

--- src/components/CourseGoal/CourseInput.jsx
+++ src/components/CourseGoal/CourseInput.jsx
@@ -1,20 +1,33 @@
 import styles from "./CourseInput.module.css";
 import Button from "../UI/Button";
+import { useState } from "react";
 
 const CourseInput = (props) => {
+  const [enterdUsername, setEnterdUsername] = useState("");
+  const [enterdAge, setEnterdAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enterdAge, enterdUsername);
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnterdUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnterdAge(event.target.value);
   };
 
   return (
     <form onSubmit={addUserHandler}>
       <div className={styles.form_control}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <input type="text" id="username" onChange={usernameChangeHandler} />
       </div>
       <div className={styles.form_control}>
         <label htmlFor="age">Age (Years) </label>
-        <input type="number" id="age" />
+        <input type="number" id="age" onChange={ageChangeHandler} />
       </div>
       <Button type="submit">Add User</Button>
     </form>
