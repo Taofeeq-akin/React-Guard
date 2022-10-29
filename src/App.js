60c93c87afc965038d2388add3377d705d2b868a
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-10-29 17:33:27 +0100

    Style App css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
-
+      <div className={styles.user_details}></div>
     </div>
   );
 }
