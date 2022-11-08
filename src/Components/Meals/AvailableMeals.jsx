74c4da0193ae2d95b7d047ce5dedb4e8be73667c
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-11-08 14:33:56 +0100

    Add Card wrapper

--- src/Components/Meals/AvailableMeals.jsx
+++ src/Components/Meals/AvailableMeals.jsx
@@ -1,5 +1,7 @@
 import classes from "./AvailableMeals.module.css";
 
+import Card from "../UI/Card";
+
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -28,13 +30,15 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li> )
+  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
 
-    return <section className={classes.meal}>
-        <ul>
-            {mealsList}
-        </ul>
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
+  );
 };
 
 export default AvailableMeals;
