adab3ee43ed023240b832f60ea4e583382be2bb4
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-11-08 11:20:01 +0100

    Add cart btn component

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+
 import classes from "./Header.module.css";
 import mealImage from "../assets/meals.jpg";
+import HeaderCartBtn from "./HeaderCartBtn";
 
 const Header = () => {
   return (
     <React.Fragment>
-      <header className={classes.Header}>
+      <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartBtn />
       </header>
-      <div className={classes.main - image}>
+      <div className={classes.main_image}>
         <img src={mealImage} alt="Delicious food table" />
       </div>
     </React.Fragment>
---
+++ src/Components/Layout/HeaderCartBtn.jsx
@@ -0,0 +1,14 @@
+import classes from "./HeaderCartBtn.module.css";
+
+const HeaderCartBtn = (props) => {
+  return (
+    <button className={classes["button"]}>
+      <span className={classes.icon}> </span>
+      <span>Your Cart</span>
+      <span className={classes["badge"]}>3</span>
+    </button>
+  );
+};
+
+export default HeaderCartBtn;
+ 
