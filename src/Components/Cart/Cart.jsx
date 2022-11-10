d4a1a6e76d48e237b501f0e554bd47d4aea8cfa2
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-11-10 15:46:02 +0100

    Add cart item file for restyle other functions

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,17 +2,30 @@ import React from "react";
 import Modal from "./Modal";
 import classes from "./Cart.module.css";
 import CartContext from "../store/Cart-context";
+import CartItem from "./CartItem";
+
 import { useContext } from "react";
 
 const Cart = (props) => {
-  const cartCtx = useContext(CartContext)
-  const totalAmount = cartCtx.totalAmount.toFixed(2)
-  const hasItem = cartCtx.items.length > 0
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = cartCtx.totalAmount.toFixed(2);
+  const hasItem = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddhandler = (item) => {};
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)} //Binding ensure that the id is passed to the remove funtion
+          onAddd={cartItemAddhandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
