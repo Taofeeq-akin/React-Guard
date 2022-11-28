dace6793fb9258d3b80942b93741f3bbfae5485e
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-11-28 11:11:11 +0100

    Use Error Boundares to catch error

--- src/components/UserFinder.jsx
+++ src/components/UserFinder.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import ErrorBoundary from "./ErrorBoundary";
 import classes from "./UserFinder.module.css";
 import Users from "./Users";
 
@@ -45,7 +46,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </>
     );
   }
