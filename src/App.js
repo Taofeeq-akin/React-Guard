d586066f1a6ea77e27b170cacbf21d2d07ea2134
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-12-01 11:04:04 +0100

    Sending http rquest using fetch

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  // const dummyMovies = [
+  //   {
+  //     id: 1,
+  //     title: 'Some Dummy Movie',
+  //     openingText: 'This is the opening text of the movie',
+  //     releaseDate: '2021-05-18',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Some Dummy Movie 2',
+  //     openingText: 'This is the second opening text of the movie',
+  //     releaseDate: '2021-05-19',
+  //   },
+  // ];
+
+  const [movies, setMovies] = useState([]);
+
+  // Using fetch to connect to backend api
+  function fetchMoviesHandler() {
+    fetch("https://swapi.dev/api/films/")
+      .then((respond) => {
+        return respond.json();
+      })
+      .then((data) => {
+        // Transforming key name to props name used in my movieList
+        const transformedMovies = data.results.map((moviesData) => {
+          return {
+            id: moviesData.episode_id,
+            title: moviesData.title,
+            releaseDate: moviesData.release_Date,
+            openingText: moviesData.opening_crawl,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
