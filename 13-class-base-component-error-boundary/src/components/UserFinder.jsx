53810b773b20376348836462472081332dea9325
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-12-13 13:31:19 +0100

    Working with form & user input

---
+++ 13-class-base-component-error-boundary/src/components/UserFinder.jsx
@@ -0,0 +1,81 @@
+import { Component } from "react";
+import ErrorBoundary from "./ErrorBoundary";
+import classes from "./UserFinder.module.css";
+import Users from "./Users";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+
+class UserFinder extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // filteredUsers: DUMMY_USERS,
+      filteredUsers: [],
+      searchTerm: "",
+    };
+  }
+
+  // Will only run once wen the page is rendered mostly use for sending http request...
+  componentDidMount() {
+    // send http request
+    this.setState({ filteredUsers: DUMMY_USERS });
+  }
+
+  //   used instead of useEffect
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
+      </>
+    );
+  }
+}
+
+// const UserFinder = () => {
+//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+//   const [searchTerm, setsearchTerm] = useState("");
+
+//   useEffect(() => {
+//     setFilteredUsers(
+//       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+//     );
+//   }, [searchTerm]);
+
+//   const searchChangeHandler = (event) => {
+//     setsearchTerm(event.target.value);
+//   };
+
+//   return (
+//     <>
+//       <div className={classes.finder}>
+//         <input type="search" onChange={searchChangeHandler} />
+//       </div>
+//       <Users users={filteredUsers} />
+//     </>
+//   );
+// };
+
+export default UserFinder;
---
+++ 14-sending-http-request-&-connecting-to-a-database/src/App.js
@@ -0,0 +1,102 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // Using fetch to connect to backend api
+  // will use callBack cus fetchMoviesHandler will give infinite loop in useEffect
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      // but since fetch will return promise i will use async function
+      const respond = await fetch(
+        "https://react-movie-http-8779a-default-rtdb.firebaseio.com/movies.json"
+      ); // Added movie.json cus it have to be addded wen using firbase
+
+      if (!respond.ok) {
+        throw new Error("Something went wrong");
+      }
+      const data = await respond.json();
+
+      const loadedMovies = [];
+
+      // i use for loop cus data return an object
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      // fetch get data from swapi api
+
+      // Transforming key name to props name used in my movieList
+      // const moviesLoaded = data.result.map((movieData) => {
+      //   return {
+      //     title: movieData.title,
+      //     id: movieData.episode_id,
+      //     releasedDate: releaseDate.release_date,
+      //   };
+      // });
+
+      // setMovies(moviesLoaded)
+      setMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  // Will need useEffect cus fetch also gives a side effect
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  const addMoviesHandler = async (movie) => {
+    // Fetch can also be used to send data
+    const response = await fetch(
+      "https://react-movie-http-8779a-default-rtdb.firebaseio.com/movies.json",
+      {
+        // use to configure the out going effect
+        method: "POST",
+        body: JSON.stringify(movie), // use JSON object cus body want what its taking in json format
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+    fetchMoviesHandler();
+  };
+
+  let content = <p>Found no movie yet.</p>;
+  if (movies.length > 0) content = <MoviesList movies={movies} />;
+  if (error) content = <p>{error}</p>;
+  if (isLoading) content = <p>Loading...</p>;
+
+  return (
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMoviesHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
+  );
+}
+
+export default App;
