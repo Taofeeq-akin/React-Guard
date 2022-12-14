5414c047f74825f48e3918bdd092d984f57c6845
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-12-14 01:13:41 +0100

    Getting input values

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,39 @@
+import { useRef } from "react";
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const nameInputRef = useRef();
+
+  const inputEnteredHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // TO clear value input after submit
+    setEnteredName("");
+
+    // can also be done like while using useRef  --But not reallly adviceable
+    // nameInputRef.current.value = "";
+    console.log(enteredName);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={inputEnteredHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
